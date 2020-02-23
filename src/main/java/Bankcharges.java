8feeeb7c11a4ed0c7826eeccd1d2706a961beb44
mason <mason@192.168.1.21>
2020-02-22 22:56:11 -0600

    Bankcharges loop and output

---
+++ src/main/java/Bankcharges.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mason
+ *
+ */
+import java.util.*;
+
+public class Bankcharges {
+
+    public static void main(String[] args) {
+
+        //ask the number of checks they have used
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter the amount of checks that you have used in the past month?");
+
+        int numberofchecks = keyboard.nextInt();
+        double totalmonthlycharges;
+        
+        if (numberofchecks <= 20){
+            totalmonthlycharges = (numberofchecks * 0.1) + 10;
+            System.out.println("Your monthly charges for " + numberofchecks + " checks is, " + totalmonthlycharges);
+        } 
+        else if (numberofchecks >= 21 && numberofchecks >= 39){
+            totalmonthlycharges = (numberofchecks * 0.8)+ 10;
+            System.out.println("Your monthly charges for " + numberofchecks + " checks is, " + totalmonthlycharges);
+        }
+        else if (numberofchecks >= 40 && numberofchecks >= 59){
+            totalmonthlycharges = (numberofchecks * 0.06) + 10;
+            System.out.println("Your monthly charges for " + numberofchecks + " checks is, " + totalmonthlycharges);
+        } 
+        else if (numberofchecks >= 60){
+            totalmonthlycharges = (numberofchecks * 0.04);
+            System.out.println("Your monthly charges for " + numberofchecks + " checks is, " + totalmonthlycharges);
+
+        
+    
+    } 
+    }
